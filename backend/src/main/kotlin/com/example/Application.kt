ba03ca014aa9130ca02af236a1dd2535bfe69cb1
witbore <witbore@gmail.com>
2024-10-12 16:21:48 +0300

    Fix the error of ignoring the YAML config. Change it to .conf, moving the deployment params to the config, and replacing the main function.

--- backend/src/main/kotlin/com/example/Application.kt
+++ backend/src/main/kotlin/com/example/Application.kt
@@ -9,13 +9,9 @@ import com.example.repository.UserRepositoryImpl
 import com.example.routes.configureRouting
 import com.example.service.UserServiceImpl
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
     val config = environment.config
--- backend/src/main/kotlin/com/example/config/Databases.kt
+++ backend/src/main/kotlin/com/example/config/Databases.kt
@@ -4,9 +4,9 @@ import io.ktor.server.config.ApplicationConfig
 import org.jetbrains.exposed.sql.*
 
 fun initDatabase(config: ApplicationConfig): Database {
-    val dbUrl = config.property("db.url").getString()
-    val dbUser = config.property("db.user").getString()
-    val dbPassword = config.property("db.password").getString()
+    val dbUrl = config.property("ktor.db.url").getString()
+    val dbUser = config.property("ktor.db.user").getString()
+    val dbPassword = config.property("ktor.db.password").getString()
 
     var database = Database.connect(
         dbUrl,
