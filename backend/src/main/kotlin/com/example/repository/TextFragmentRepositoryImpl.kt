74bcfe4d73a09fc22705c7df83d9db349d7733c4
witbore <witbore@gmail.com>
2024-10-14 22:38:49 +0300

    Add files for the text fragments. Since there is no need for working with them as separate entities, I do not create the routes.

---
+++ backend/src/main/kotlin/com/example/repository/TextFragmentRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.example.repository
+
+import com.example.model.TextFragment
+
+class TextFragmentRepositoryImpl : TextFragmentRepository {
+    override suspend fun getTextFragmentById(id: Int): TextFragment? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addTextFragment(textFragment: TextFragment) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateTextFragment(textFragment: TextFragment) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteTextFragmentById(id: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllTextFragments(): List<TextFragment> {
+        TODO("Not yet implemented")
+    }
+}
