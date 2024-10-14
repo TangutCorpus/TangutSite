dda654b1531561d77e4cac050f4024f0f6ddcc9f
witbore <witbore@gmail.com>
2024-10-14 23:02:57 +0300

    Add Text Fragment Service implementation.

--- backend/src/main/kotlin/com/example/service/TextFragmentService.kt
+++ backend/src/main/kotlin/com/example/service/TextFragmentService.kt
@@ -3,7 +3,7 @@ package com.example.service
 import com.example.model.TextFragment
 
 interface TextFragmentService {
-    suspend fun getTextFragmentById(id: Int) : TextFragment?;
+    suspend fun getTextFragmentById(id: Int): TextFragment?;
     suspend fun addTextFragment(textFragment: TextFragment);
     suspend fun updateTextFragment(textFragment: TextFragment);
     suspend fun deleteTextFragmentById(id: Int);
--- backend/src/main/kotlin/com/example/service/TextFragmentServiceImpl.kt
+++ backend/src/main/kotlin/com/example/service/TextFragmentServiceImpl.kt
@@ -1,25 +1,29 @@
 package com.example.service
 
 import com.example.model.TextFragment
+import com.example.repository.TextFragmentRepository
 
-class TextFragmentServiceImpl: TextFragmentService {
+class TextFragmentServiceImpl(private val fragmentRepository: TextFragmentRepository) : TextFragmentService {
     override suspend fun getTextFragmentById(id: Int): TextFragment? {
-        TODO("Not yet implemented")
+        require(id >= 0) { "ID cannot be negative" }
+        return fragmentRepository.getTextFragmentById(id)
+            ?: throw NoSuchElementException("No fragment found with id $id")
     }
 
     override suspend fun addTextFragment(textFragment: TextFragment) {
-        TODO("Not yet implemented")
+        require(textFragment.content.isNotBlank() && textFragment.textId >= 0) { "Fragment cannot be empty" }
+        fragmentRepository.addTextFragment(textFragment)
     }
 
     override suspend fun updateTextFragment(textFragment: TextFragment) {
-        TODO("Not yet implemented")
+        require(textFragment.content.isNotBlank() && textFragment.textId >= 0) { "Fragment cannot be empty" }
+        fragmentRepository.updateTextFragment(textFragment)
     }
 
     override suspend fun deleteTextFragmentById(id: Int) {
-        TODO("Not yet implemented")
+        require(id >= 0) { "ID cannot be negative" }
+        fragmentRepository.deleteTextFragmentById(id)
     }
 
-    override suspend fun getAllTextFragments(): List<TextFragment> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getAllTextFragments(): List<TextFragment> = fragmentRepository.getAllTextFragments()
 }
