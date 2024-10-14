9304fa331e99f9a4d6804a50562553c2c8dbf3e0
witbore <witbore@gmail.com>
2024-10-14 20:21:02 +0300

    Add Text Service implementation.

--- backend/src/main/kotlin/com/example/service/TextServiceImpl.kt
+++ backend/src/main/kotlin/com/example/service/TextServiceImpl.kt
@@ -1,33 +1,30 @@
 package com.example.service
 
 import com.example.model.Text
+import com.example.repository.TextRepository
 
-class TextServiceImpl : TextService {
+class TextServiceImpl(private val textRepository: TextRepository) : TextService {
+
     override suspend fun getTextById(id: Int): Text? {
-        TODO("Not yet implemented")
+        require(id >= 0) { "ID cannot be negative" }
+        return textRepository.getTextById(id) ?: throw NoSuchElementException("No text found with id $id")
     }
 
     override suspend fun addText(text: Text) {
-        TODO("Not yet implemented")
+        require(text.pureText.isNotBlank() && text.lineIds.isNotEmpty()) { "Text cannot be empty" }
+        textRepository.addText(text)
     }
 
     override suspend fun updateText(text: Text) {
-        TODO("Not yet implemented")
+        require(text.pureText.isNotBlank() && text.lineIds.isNotEmpty()) { "Text cannot be empty" }
+        textRepository.updateText(text)
     }
 
     override suspend fun deleteTextById(id: Int) {
-        TODO("Not yet implemented")
+        require(id >= 0) { "ID cannot be negative" }
+        textRepository.deleteTextById(id)
     }
 
-    override suspend fun getAllTexts(): List<Text> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getAllTexts(): List<Text> = textRepository.getAllTexts()
 
-    override suspend fun updateTextCommentById(id: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun updateTextContentById(id: Int) {
-        TODO("Not yet implemented")
-    }
 }
