0107236d5ea2f0003a28973967d0bddd60d305b7
witbore <witbore@gmail.com>
2024-10-14 23:02:57 +0300

    Add Text Fragment Service implementation.

--- backend/src/main/kotlin/com/example/repository/TextFragmentRepository.kt
+++ backend/src/main/kotlin/com/example/repository/TextFragmentRepository.kt
@@ -4,9 +4,9 @@ import com.example.model.TextFragment
 import org.jetbrains.exposed.sql.statements.InsertStatement
 
 interface TextFragmentRepository {
-    suspend fun getTextFragmentById(id: Int): TextFragment?;
-    suspend fun addTextFragment(textFragment: TextFragment): InsertStatement<Number>;
-    suspend fun updateTextFragment(textFragment: TextFragment): Int;
-    suspend fun deleteTextFragmentById(id: Int): Int;
-    suspend fun getAllTextFragments(): List<TextFragment>;
+    suspend fun getTextFragmentById(id: Int): TextFragment?
+    suspend fun addTextFragment(textFragment: TextFragment): InsertStatement<Number>
+    suspend fun updateTextFragment(textFragment: TextFragment): Int
+    suspend fun deleteTextFragmentById(id: Int): Int
+    suspend fun getAllTextFragments(): List<TextFragment>
 }
