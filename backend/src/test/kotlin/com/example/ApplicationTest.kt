dd1e78b6ef36f072e250ec98b04b99a651c3cbe8
witbore <witbore@gmail.com>
2024-10-29 18:43:04 +0300

    Add main page route test.

--- backend/src/test/kotlin/com/example/ApplicationTest.kt
+++ backend/src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,12 +1,15 @@
 package com.example
 
+import com.example.routes.MainPageRoutesTest
 import kotlin.test.Test
 
 open class ApplicationTest {
 
     @Test
     fun runAllTests() {
-
+        MainPageRoutesTest().apply{
+            `test GET main page returns OK`()
+        }
     }
 }
 
