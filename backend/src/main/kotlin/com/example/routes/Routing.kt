601640c97ed81d2cc9b4d0265f6b4daba7ca0801
witbore <witbore@gmail.com>
2024-10-29 16:59:04 +0300

    Fix double StatusPage plugin installation error: after initialisation of the project, the old plugin installation wasn't deleted.

--- backend/src/main/kotlin/com/example/routes/Routing.kt
+++ backend/src/main/kotlin/com/example/routes/Routing.kt
@@ -2,18 +2,11 @@ package com.example.routes
 
 import com.example.service.TextService
 import com.example.service.UserService
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting(userService: UserService, textService: TextService) {
-    install(StatusPages) {
-        exception<Throwable> { call, cause ->
-            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
-        }
-    }
     routing {
         get("/") {
             call.respondText("Hello World!")
