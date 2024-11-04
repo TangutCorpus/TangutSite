3a08e52215efaf63ed53b19891fead9ca1f7c3ca
witbore <witbore@gmail.com>
2024-11-04 16:55:34 +0300

    Add fragment routing to the routes.
    Forgot to add routes after implementing repository and service layers.

---
+++ backend/src/main/kotlin/com/example/routes/TextFragmentRoutes.kt
@@ -0,0 +1,8 @@
+package com.example.routes
+
+import com.example.service.TextFragmentService
+import io.ktor.server.routing.Route
+
+fun Route.textFragmentRoutes(textFragmentService: TextFragmentService) {
+
+}
