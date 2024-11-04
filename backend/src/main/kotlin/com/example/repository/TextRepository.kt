d0eba1d10c662b110cf05503c053d2f5eade181e
witbore <witbore@gmail.com>
2024-11-05 00:33:40 +0300

    Add RSQL parser for advanced search requests.

--- backend/src/main/kotlin/com/example/repository/TextRepository.kt
+++ backend/src/main/kotlin/com/example/repository/TextRepository.kt
@@ -4,6 +4,7 @@ import com.example.model.Text
 
 interface TextRepository {
     suspend fun getTextById(id: Int): Text?
+    suspend fun getTextsByQuery(query: String): List<Text>
     suspend fun addText(text: Text): Int
     suspend fun updateText(text: Text): Int
     suspend fun deleteTextById(id: Int): Int
