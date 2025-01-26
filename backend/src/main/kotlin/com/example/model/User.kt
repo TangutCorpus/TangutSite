1f8b8849319cf2fefe46e0519dd51a6e199489c7
witbore <witbore@gmail.com>
2025-01-26 19:09:54 +0300

    Rewrite the User data class.
    The previous version of the class was copied from somewhere and used as a dummy.

--- backend/src/main/kotlin/com/example/model/User.kt
+++ backend/src/main/kotlin/com/example/model/User.kt
@@ -1,6 +1,29 @@
 package com.example.model
 
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
+import java.util.UUID
+
+
+/**
+ * Represents a User.
+ *
+ * @property id Unique identifier for the user.
+ * @property email Email address of the user, used for login and communication.
+ * @property password Encrypted password for authentication purposes.
+ * @property nickname Shorthand used for tagging and as a user page address.
+ * @property name Full name, which can be in any supported language, useful in academic and professional contexts.
+ * @property biography Descriptive text outlining the user's achievements and providing links to external resources.
+ * @property role The role of the registered user, which determines access and permissions. Possible values include "Editor", "Moderator", and "Administrator".
+ */
 
 @Serializable
-data class User(val name: String, val age: Int)
+data class User(
+    @Contextual val id: UUID = UUID.randomUUID(),
+    val nickname: String,
+    val email: String,
+    val password: String,
+    val name: String,
+    val biography: String,
+    val role: String
+)
