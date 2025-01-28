36b65cc523ea722a1cdfa2c4fee0f059135c6e6c
witbore <witbore@gmail.com>
2025-01-28 18:28:28 +0300

    Delete password and email checks.
     I plan to move them to the service layer in the future.

--- backend/src/main/kotlin/com/example/model/Users.kt
+++ backend/src/main/kotlin/com/example/model/Users.kt
@@ -7,8 +7,8 @@ import org.jetbrains.exposed.sql.javatime.datetime
 object Users : Table() {
     val id = uuid("id")
     val nickname = text("nickname").check { it.charLength() less 255 }
-    val email = text("email").check { it.charLength() less 255 }
-    val password = text("password").check { it.charLength() less 255 }
+    val email = text("email")
+    val password = text("password")
     val name = text("name").check { it.charLength() less 255 }
     val biography = text("biography")
     val role = text("role")
