22fc7608566c02d7bb998b506f0122d149966eff
witbore <witbore@gmail.com>
2025-01-31 15:06:01 +0300

    Code refactoring.
    Delete auto-generated files. Reformat files. Extract where possible code to separate components. Redesign 404 page.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="w-full flex flex-col mt-[15%] items-center">
+    <h1 class="first-title font-bold text-6xl mb-2">Tangut</h1>
+    <p class="text-gray-600 mb-4 text-center">
+      Онлайн-библиотека и словарь тангутского языка
+    </p>
+    <SearchBar />
+  </div>
 </template>
+
+<script setup lang="ts">
+import SearchBar from '../components/SearchBar.vue'
+</script>
