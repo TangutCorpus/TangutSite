5accc8f2bc8014602fbba062ad3e8b6bdc1052a2
witbore <witbore@gmail.com>
2025-02-04 00:14:08 +0300

    Create folder for helpers

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
-import router from '@/router'
+import router from 'src/helpers/router'
 import '@/assets/styles/main.css'
 
 const app = createApp(App)
