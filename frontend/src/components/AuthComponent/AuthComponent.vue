9a29fb5dbef83c6f32c8806cb31dd60a1cdeb511
witbore <witbore@gmail.com>
2025-02-06 21:22:42 +0300

    Adjust frontend auth design.

--- frontend/src/components/AuthComponent/AuthComponent.vue
+++ frontend/src/components/AuthComponent/AuthComponent.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center" style="z-index: 50;">
-    <div class="bg-white rounded-md shadow-lg relative w-[40vw] h-[55vh] flex">
-      <div class="left-side rounded-l w-2/5 p-8 text-white bg-black">
+  <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4" style="z-index: 50;">
+    <div class="bg-white rounded-md shadow-lg relative w-[50vw] h-[55vh] max-h-[90vh] flex">
+      <div class="left-side rounded-l w-2/5 p-8 text-white bg-black flex flex-col justify-center">
         <h1 class="text-5xl font-bold mb-5">𗼇𗟲</h1>
         <p class="mb-4">
           Зарегистрируйтесь или войдите, чтобы получить полный доступ к библиотеке, словарю и грамматике.
         </p>
       </div>
-      <div class="right-side rounded-md w-3/5 p-8 pr-8 relative">
+
+      <div class="right-side rounded-md w-3/5 p-8 pr-8 relative overflow-y-auto max-h-[90vh]">
         <button class="absolute top-2 right-2 text-gray-500 hover:text-gray-700" @click="handleClose">
           <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M6 18L18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
           </svg>
         </button>
+
         <h2 class="text-5xl font-bold mb-4">
           {{ mode === 'register' ? (step === 1 ? 'Регистрация' : 'Регистрация') : 'Вход' }}
         </h2>
@@ -28,31 +30,28 @@
           </button>
         </div>
 
-        <form @submit.prevent="handleSubmit">
+        <form class="space-y-4" @submit.prevent="handleSubmit">
           <div v-if="mode === 'register' && step === 1">
             <div class="mb-4">
               <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email</label>
               <input id="email" v-model="email"
                      class="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                     required
-                     type="email" />
+                     required type="email" />
             </div>
             <div class="mb-4">
               <label class="block text-gray-700 text-sm font-bold mb-2" for="nickname">Никнейм</label>
               <input id="nickname" v-model="username"
                      class="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                     required
-                     type="text" />
+                     required type="text" />
             </div>
             <div class="mb-4">
               <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Пароль</label>
               <input id="password" v-model="password"
                      class="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                     required
-                     type="password" />
+                     required type="password" />
             </div>
             <button
-              class="bg-black text-white px-4 py-2 rounded hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 absolute bottom-4 right-4"
+              class="bg-black text-white px-4 py-2 rounded hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
               type="submit">
               Дальше
             </button>
@@ -63,8 +62,7 @@
               <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Ф.И.О.</label>
               <input id="name" v-model="name"
                      class="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                     required
-                     type="text" />
+                     required type="text" />
             </div>
             <div class="mb-6">
               <label class="block text-gray-700 text-sm font-bold mb-2" for="biography">О себе</label>
@@ -77,7 +75,7 @@
               <span class="text-sm">Я согласен с условиями</span>
             </div>
             <button
-              class="bg-black text-white px-4 py-2 rounded hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 absolute bottom-4 right-4"
+              class="bg-black text-white px-4 py-2 rounded hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
               type="submit">
               Завершить регистрацию
             </button>
@@ -88,18 +86,16 @@
               <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email</label>
               <input id="email" v-model="email"
                      class="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                     required
-                     type="email" />
+                     required type="email" />
             </div>
             <div class="mb-4">
               <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Пароль</label>
               <input id="password" v-model="password"
                      class="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                     required
-                     type="password" />
+                     required type="password" />
             </div>
             <button
-              class="bg-black text-white px-4 py-2 rounded hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 absolute bottom-4 right-4"
+              class="bg-black text-white px-4 py-2 rounded hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
               type="submit">
               Войти
             </button>
