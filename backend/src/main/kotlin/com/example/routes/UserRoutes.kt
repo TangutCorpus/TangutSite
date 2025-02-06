88680b8c6c47177f2db8dd2440dd4c32126915c0
witbore <witbore@gmail.com>
2025-02-06 21:03:20 +0300

    Disable RB-authorization for simplicity of testing.

--- backend/src/main/kotlin/com/example/routes/UserRoutes.kt
+++ backend/src/main/kotlin/com/example/routes/UserRoutes.kt
@@ -1,9 +1,7 @@
 package com.example.routes
 
 import com.example.model.ExposedUser
-import com.example.model.UserRoles
 import com.example.model.updateFromExposedUser
-import com.example.plugins.hasAnyRole
 import com.example.service.UserService
 import com.example.utils.toUUIDOrNull
 import io.ktor.http.HttpStatusCode
@@ -12,36 +10,34 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.userRoutes(userService: UserService) {
-    hasAnyRole(UserRoles.EDITOR) {
-        get("/users/{id}") {
-            val id = call.parameters["id"]?.toUUIDOrNull()
-            userService.getUserById(id)?.let { user -> call.respond(HttpStatusCode.OK, user) }
-                ?: call.respondText("User not modified", status = HttpStatusCode.NotModified)
-        }
+    get("/users/{id}") {
+        val id = call.parameters["id"]?.toUUIDOrNull()
+        userService.getUserById(id)?.let { user -> call.respond(HttpStatusCode.OK, user) }
+            ?: call.respondText("User not modified", status = HttpStatusCode.NotModified)
+    }
 
-        put("/users/{id}") {
-            val id = call.parameters["id"]?.toUUIDOrNull()
-            val receivedUser = call.receive<ExposedUser>()
-            var user = userService.getUserById(id)?.updateFromExposedUser(receivedUser)
-            if (userService.updateUser(user)) {
-                call.respondText("User updated successfully", status = HttpStatusCode.OK)
-            } else {
-                call.respondText("User not modified", status = HttpStatusCode.NotModified)
-            }
+    put("/users/{id}") {
+        val id = call.parameters["id"]?.toUUIDOrNull()
+        val receivedUser = call.receive<ExposedUser>()
+        var user = userService.getUserById(id)?.updateFromExposedUser(receivedUser)
+        if (userService.updateUser(user)) {
+            call.respondText("User updated successfully", status = HttpStatusCode.OK)
+        } else {
+            call.respondText("User not modified", status = HttpStatusCode.NotModified)
         }
+    }
 
-        delete("/users/{id}") {
-            val id = call.parameters["id"]?.toUUIDOrNull()
-            if (userService.deleteUserById(id)) {
-                call.respondText("User deleted successfully", status = HttpStatusCode.OK)
-            } else {
-                call.respondText("User not deleted", status = HttpStatusCode.NotModified)
-            }
+    delete("/users/{id}") {
+        val id = call.parameters["id"]?.toUUIDOrNull()
+        if (userService.deleteUserById(id)) {
+            call.respondText("User deleted successfully", status = HttpStatusCode.OK)
+        } else {
+            call.respondText("User not deleted", status = HttpStatusCode.NotModified)
         }
+    }
 
-        get("/users") {
-            val users = userService.getAllUsers()
-            call.respond(HttpStatusCode.OK, users)
-        }
+    get("/users") {
+        val users = userService.getAllUsers()
+        call.respond(HttpStatusCode.OK, users)
     }
 }
