2f11845afe6daa5ea4cebd2ebd8fe9122e6bb9b8
witbore <witbore@gmail.com>
2025-02-06 21:14:47 +0300

    Create model for signup request.
    It is inconvenient to pass the password in a different way for the frontend. To strengthen the security, this is planned to be changed in the future.

---
+++ backend/src/main/kotlin/com/example/model/SignupRequest.kt
@@ -0,0 +1,13 @@
+package com.example.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SignupRequest(
+    val email: String,
+    val username: String,
+    val password: String,
+    val avatarUrl: String = "",
+    val displayName: String = "",
+    val biography: String = ""
+)
