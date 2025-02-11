701e0ed48e3048e93fc5fb1aa234d5863e198030
witbore <witbore@gmail.com>
2025-02-11 13:26:01 +0300

    Add max file size constraint

--- backend/src/main/kotlin/com/example/config/Serialization.kt
+++ backend/src/main/kotlin/com/example/config/Serialization.kt
@@ -1,8 +1,15 @@
 package com.example.config
 
-import io.ktor.serialization.kotlinx.json.*
+import io.ktor.http.content.PartData
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.dataconversion.DataConversion
+import io.ktor.server.plugins.partialcontent.PartialContent
+import io.ktor.server.plugins.requestvalidation.RequestValidation
+import io.ktor.server.plugins.requestvalidation.ValidationResult
+import io.ktor.utils.io.readRemaining
+import kotlinx.io.readByteArray
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
@@ -15,16 +22,32 @@ import kotlinx.serialization.modules.SerializersModule
 import java.util.UUID
 
 @OptIn(ExperimentalSerializationApi::class)
-fun Application.configureSerialization() {
+val jsonConfig = Json {
+    serializersModule = SerializersModule {
+        contextual(UUID::class, UUIDSerializer)
+    }
+    isLenient = true
+    prettyPrint = true
+    ignoreUnknownKeys = true
+}
+
+fun Application.configureSerialization(maxFileSize: Int) {
     install(ContentNegotiation) {
-        json(Json {
-            serializersModule = SerializersModule {
-                contextual(UUID::class, UUIDSerializer)
+        json(jsonConfig)
+    }
+
+    install(PartialContent)
+
+    install(DataConversion)
+
+    install(RequestValidation) {
+        validate<PartData.FileItem> { part ->
+            val fileSize = part.provider().readRemaining().readByteArray().size
+            if (fileSize > maxFileSize) {
+                return@validate ValidationResult.Invalid("File is too big: $fileSize bytes")
             }
-            isLenient = true
-            prettyPrint = true
-            ignoreUnknownKeys = true
-        })
+            ValidationResult.Valid
+        }
     }
 }
 
