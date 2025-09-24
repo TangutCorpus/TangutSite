d0f2ccca6122d893199f5413c769b729ff822f5a
witbore <witbore@gmail.com>
2025-09-25 01:27:35 +0300

    Add font for Tangut langauge

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,15 @@
-        /** @type {import('tailwindcss').Config} */
-        module.exports = {
-          content: [
-              "./index.html",
-              "./src/**/*.{vue,js,ts,jsx,tsx}",
-          ],
-          theme: {
-              extend: {},
-          },
-          plugins: [],
-          }
-  
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                tangut: ['TangutYinchuan', 'serif'], // fallback to serif
+            },
+        },
+    },
+    plugins: [],
+}
